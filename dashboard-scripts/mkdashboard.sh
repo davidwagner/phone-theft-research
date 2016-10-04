f908a05a96567fe2e16e8ac334d336c4e5d757d1
David Wagner <daw@cs.berkeley.edu>
2016-10-03 21:40:10 -0700

    Update dashboard script to only use last 2 days

--- dashboard-scripts/mkdashboard.sh
+++ dashboard-scripts/mkdashboard.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-cd ~/phone-theft-research/dashboard-scripts/
+
+rm -r /var/tmp/encrypted
+mkdir -p /var/tmp/encrypted
+cd ~/Dropbox/phone_data/Sensor\ Research
+for BEFORE in 0 1 2
+do
+    DAY=`python -c "import datetime; d = datetime.datetime.now() - datetime.timedelta(${BEFORE}); print d.strftime('%Y_%m_%d')"`
+    cp *_${DAY}_* /var/tmp/encrypted/
+done
+
 rm -r /var/tmp/decrypted
 mkdir -p /var/tmp/decrypted
-python bintocsv.py ucb_keypair/ucb.privatekey -s ~/Dropbox/phone_data/Sensor\ Research -d /var/tmp/decrypted/
+
+cd ~/phone-theft-research/dashboard-scripts/
+python bintocsv.py ucb_keypair/ucb.privatekey -s /var/tmp/encrypted -d /var/tmp/decrypted/
 python logchecker.py /var/tmp/decrypted
