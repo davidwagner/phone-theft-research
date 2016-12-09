65130a4a2d32fae01c6c10f0fdb94c8de001c5af
Xinyu Liu <xinyuliu@berkeley.edu>
2016-12-09 01:41:24 -0800

    random forest has highest accuracy. switch classifier

--- classifiers-scripts/TheftClassifier.py
+++ classifiers-scripts/TheftClassifier.py
@@ -5,7 +5,7 @@ import BaseClassifier
 
 from sklearn.externals import joblib
 
-filename = './data/theft_classifiers_weights/linear_svm_weights.pkl'
+filename = './data/theft_classifiers_weights/random_forest_weights.pkl'
 clf = joblib.load(filename)
 class Classifier(BaseClassifier.BaseClassifier):
 
