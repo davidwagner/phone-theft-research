cd03f9891ebb6f3c9ae6b292dc02a383146982e6
David Wagner <daw@cs.berkeley.edu>
2016-12-13 12:52:50 -0800

    Move config settings into a separate file. Remove duplicate copy of classifier picked state.

--- classifiers-scripts/ClassifierLog.py
+++ classifiers-scripts/ClassifierLog.py
@@ -11,12 +11,8 @@ from matplotlib.dates import SecondLocator, MinuteLocator, HourLocator, DateForm
 import Sensors as sensors
 import Classifiers as classifiers 
 
-##### CONFIGURE FOR OWN MACHINE ###############
-# Replace with your own
-DASHBOARDDIR = '../data/Classifier_Results_TEST/'
-USERS_TO_IDS_FILE = "../data/users_r2_test.csv"
-DIRECTORY = "../data/Decrypted_Data/2016_11_01/"
-###############################################
+from configsettings import *
+
 DUMP_RESULTS = True
 
 if DIRECTORY[-1] != '/':
---
+++ classifiers-scripts/configsettings.py
@@ -0,0 +1,6 @@
+##### CONFIGURE FOR OWN MACHINE ###############
+# Replace with your own
+DASHBOARDDIR = '../data/Classifier_Results_TEST/'
+USERS_TO_IDS_FILE = "../data/users_r2_test.csv"
+DIRECTORY = "../data/Decrypted_Data/2016_11_01/"
+###############################################
