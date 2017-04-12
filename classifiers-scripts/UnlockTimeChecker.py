06fc71f34ffb7a54c9636a6dca483dbec8f29544
Steven Chen <sccchenster@gmail.com>
2017-04-11 17:24:57 -0700

    Add test

--- classifiers-scripts/UnlockTimeChecker.py
+++ classifiers-scripts/UnlockTimeChecker.py
@@ -24,7 +24,7 @@ def computeUnlocks(keyguardData, activationIntervals):
 				if intervalIndex >= len(activationIntervals):
 					return (unlocksSaved, totalUnlocks, savedTimes)
 
-			if unlockTime >= activationIntervals[intervalIndex][0] and unlockTime <= activationIntervals[intervalIndex][1]
+			if unlockTime >= activationIntervals[intervalIndex][0] and unlockTime <= activationIntervals[intervalIndex][1]:
 				unlocksSaved += 1
 				savedTimes.append(unlockTime)
 
@@ -32,3 +32,11 @@ def computeUnlocks(keyguardData, activationIntervals):
 
 
 	return (unlocksSaved, totalUnlocks, savedTimes)
+
+def time(hours, minutes, seconds):
+	return datetime(2017, 4, 11, hour=hours, minute=minutes, second=seconds)
+
+if __name__ == '__main__':
+	test_data = [[time(8, 0, 0), "true"], [time(8, 50, 10), "false"], [time(12, 0, 0), "true"], [time(12, 50, 10), "false"]]
+	test_intervals = [(time(8, 40, 0), time(9, 10, 0)), (time(13, 40, 0), time(14, 10, 0))]
+	print(computeUnlocks(test_data, test_intervals))
