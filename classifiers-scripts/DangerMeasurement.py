a2ea7f91cf008160c2f01533888148014a10ca63
Steven Chen <sccchenster@gmail.com>
2017-04-23 21:55:53 -0700

    Do multiple days for dangermeasure

--- classifiers-scripts/DangerMeasurement.py
+++ classifiers-scripts/DangerMeasurement.py
@@ -44,38 +44,41 @@ def continousIntervals(userData):
 	return intervals
 
 if __name__ == '__main__':
-	count = 0
-	NOW = datetime.datetime.now()
-	NOW_TIME = NOW.strftime('_%m_%d_%H_%M')
-	summaryFile = open('bt-connected-summary-' + DATA_DATES[0] + '_' + NOW_TIME +'.txt', 'w+')
-	summaryWriter = csv.writer(summaryFile)
-	summaryWriter.writerow(["User", "BT Connection Changed", "Percent Dropped", "Percent Connected", "Phone Model"])
-	allUsers = []
-	for USER_ID in USERS:
-		count += 1
-		print("Number of users processed:", count)
-		print("Currently on:", USER_ID)
-		cl.DIRECTORY = DIRECTORY + "/" + DATA_DATES[0] + "/"
-		try :
-			dataFiles = cl.getUserFilesByDayAndInstrument(USER_ID, sensors.CONNECTED_DEVICES)
-			userData = cl.dataFilesToDataListAbsTime(dataFiles)
-			#print(userData)
+	for DATA_DAY in DATA_DATES:
+		count = 0
+		NOW = datetime.datetime.now()
+		NOW_TIME = NOW.strftime('_%m_%d_%H_%M')
+		summaryFile = open('bt-connected-summary-' + DATA_DAY + '_' + NOW_TIME +'.csv', 'w+')
+		logFile = open('bt-connected-log-' + DATA_DAY + '_' + NOW_TIME +'.txt', 'w+')
+		summaryWriter = csv.writer(summaryFile)
+		summaryWriter.writerow(["User", "BT Connection Changed", "Percent Dropped", "Percent Connected", "Phone Model"])
+		allUsers = []
+		print("On day:", str(DATA_DAY))
+		for USER_ID in USERS:
+			count += 1
+			print("Number of users processed:", count)
+			print("Currently on:", USER_ID)
+			cl.DIRECTORY = DIRECTORY + "/" + DATA_DAY + "/"
+			try :
+				dataFiles = cl.getUserFilesByDayAndInstrument(USER_ID, sensors.CONNECTED_DEVICES)
+				userData = cl.dataFilesToDataListAbsTime(dataFiles)
+				#print(userData)
 
-			intervals = continousIntervals(userData)
-			freq = len(intervals) - 1
-			percentConnected, percentDropped, totalTime = stats(intervals)
-			phoneModel = ""
-			allUsers.append((USER_ID, freq, percentDropped, percentConnected, phoneModel))
+				intervals = continousIntervals(userData)
+				freq = len(intervals) - 1
+				percentConnected, percentDropped, totalTime = stats(intervals)
+				phoneModel = ""
+				allUsers.append((USER_ID, freq, percentDropped, percentConnected, phoneModel))
 
-		except:
-			tb = traceback.format_exc()
-			print(tb)
-			summaryFile.write("******EXCEPTION (while computing " + USER_ID + " )*******\n")
-			summaryFile.write(tb)
-	sortedUsers = sorted(allUsers, key=lambda x: x[1])
-	for userId, freq, percentDropped, percentConnected, phoneModel in sortedUsers:
-		summaryWriter.writerow([userId, freq, percentDropped, percentConnected, phoneModel])
-	summaryFile.close()
+			except:
+				tb = traceback.format_exc()
+				print(tb)
+				logFile.write("******EXCEPTION (while computing " + USER_ID + " )*******\n")
+				logFile.write(tb)
+		sortedUsers = sorted(allUsers, key=lambda x: x[1])
+		for userId, freq, percentDropped, percentConnected, phoneModel in sortedUsers:
+			summaryWriter.writerow([userId, freq, percentDropped, percentConnected, phoneModel])
+		summaryFile.close()
 
 
 
