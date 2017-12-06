8a12529eea815c201305ba7a25b79f81f08d07db
Xinyu Liu <xinyuliu@berkeley.edu>
2017-12-06 08:35:35 -0800

    Added code to process new theft data collected on Nexus 6P
    1. featurizer_op.py: featurize theft data to pos feature vectors.
    2. cal_detect_rate.py: train classifiers using pos & neg collected on Nexus 5X
                           and compute detection rate on pos collected on Nexus 6P.
                           Intend to show our detector works w/ different phone models.

--- phone_theft_detect_code/featurizer_op.py
+++ phone_theft_detect_code/featurizer_op.py
@@ -21,7 +21,7 @@ def compute_features_per_window(window, window_norms):
 	w_mean = window_norms.mean()
 	w_std = window_norms.std()
 	w_rms = np.sqrt(np.square(window_norms).mean())
-    # w_rms = np.sqrt(np.square(window_norms).sum()) 
+    # w_rms = np.sqrt(np.square(window_norms).sum())
 
 	w_nonans = window[np.isfinite(window['norm_after'])]
 	w_arc_len = (w_nonans['norm_after'] - w_nonans['norm']).abs().sum() / w_nonans['norm'].size
@@ -56,10 +56,10 @@ def computer_features_per_file(file_name, class_label):
     # due to pos experiment noise, we crop times norm cross 40 manully
     if class_label == '1':
         if '81452402' in file_name:
-            time_exceed = np.array([161330.0, 216094.0, 287314.0, 353417.0, 416721.0, 475336.0, 535971.0, 596367.0, 656234.0, 726406.0, 
+            time_exceed = np.array([161330.0, 216094.0, 287314.0, 353417.0, 416721.0, 475336.0, 535971.0, 596367.0, 656234.0, 726406.0,
                                     785748.0, 848996.0, 909128.0, 969941.0, 1028077.0, 1092756.0, 1148382.0, 1211740.0, 1269078.0, 1329543.0,
                                     1967731.0, 2048085.0, 2136125.0, 2221992.0, 2298554.0, 2372050.0, 2444078.0, 2514334.0, 2592409.0, 2671525.0])
-        elif '2016_12_04_19_24_47' in file_name: 
+        elif '2016_12_04_19_24_47' in file_name:
             time_exceed = np.array([933876.0, 966708.0, 1013558.0, 1061703.0, 1101594.0, 1151936.0, 1195055.0, 1247129.0, 1292453.0, 1361568.0,
                                     1432105.0, 1500765.0, 1544397.0, 1595380.0, 1637371.0, 1697678.0, 1758845.0, 1822171.0, 1893906.0, 1951704.0,
                                     2018429.0, 2088872.0, 2131228.0, 2172948.0, 2228841.0, 2284761.0, 2342263.0, 2389745.0, 2439722.0, 2483499.0])
@@ -73,6 +73,19 @@ def computer_features_per_file(file_name, class_label):
                                     7216223.0, 7285226.0, 7360614.0, 7436579.0, 7500386.0, 7575378.0, 7637309.0, 7703844.0, 7766888.0, 7827128.0])
         elif '2017_10_07_15_28_57' in file_name:
             time_exceed = np.array([511982.0, 560226.0, 611987.0, 651113.0, 693133.0, 744144.0, 800825.0, 855893.0, 926166.0, 1006527.0])
+        elif '2017_12_04_20_45_34' in file_name:
+            print('Nexus 6P Exp 2.2')
+            time_exceed = np.array([6899882.0, 6975954.0, 7044713.0, 7110112.0, 7184112.0,
+                                    7249911.0, 7313394.0, 7372449.0, 7435753.0, 7504721.0,
+                                    7553743.0, 7624700.0, 7689427.0, 7757450.0, 7812858.0])
+        elif '2017_12_04_21_52_33' in file_name:
+            print('Nexus 6P Exp 2.3&2.1')
+            time_exceed = np.array([10941787.0, 10866409.0, 11045347.0, 11136426.0, 11211593.0,
+                                    11315076.0, 11393353.0, 11491042.0, 11626581.0, 11721637.0,
+                                    11802508.0, 11934919.0, 12007584.0, 12095546.0, 12158412.0,
+                                    12831481.0, 12879822.0, 12945946.0, 13010587.0, 13068502.0,
+                                    13234685.0, 13269032.0, 13398456.0, 13450762.0, 13520260.0,
+                                    13569496.0, 13632456.0, 13691345.0, 13730728.0, 13781298.0])
         else:
             print('unexpected pos file')
         # print(pd.Series(time_exceed).isin(time_crossing.tolist()))
@@ -116,21 +129,21 @@ def computer_features_per_file(file_name, class_label):
 
                     # 6.2 Concatenate to construct a data point
                     data_point = pd.concat([data_point, w])
-                else: 
+                else:
                     window_after_start = window_after_end
                     DP_IMCOMPLETE = True
                     break
 
             if not DP_IMCOMPLETE:
                 dp[file_name+'_'+str(t)] = data_point
-        
 
 
-# build a design matrix 
+
+# build a design matrix
 dp = pd.DataFrame()
-# dp = pd.DataFrame(columns=('filename_time', 'class_label', 
+# dp = pd.DataFrame(columns=('filename_time', 'class_label',
                            # 'win1_max', 'win1_mean', 'win1_std', 'win1_rms', 'win1_arc_len', 'win1_arc_len_std', 'win1_mean_abs',
-                           # 'win2_max', 'win2_mean', 'win2_std', 'win2_rms', 'win2_arc_len', 'win2_arc_len_std', 'win2_mean_abs')) 
+                           # 'win2_max', 'win2_mean', 'win2_std', 'win2_rms', 'win2_arc_len', 'win2_arc_len_std', 'win2_mean_abs'))
 # pre-allocate # of rows. keep a counter
 
 if __name__ == '__main__':
@@ -143,17 +156,18 @@ if __name__ == '__main__':
     # # neg_path = 'data/theft_classifier_data/neg_CSVs' # local
     # neg_path = 'data/theft_classifier_data/sensor_research' # remote
     # neg_csv_files = [f for f in os.listdir(neg_path) if f.endswith('.csv') and 'BatchedAccelerometer' in f]
-    # for f in neg_csv_files: 
+    # for f in neg_csv_files:
     #     computer_features_per_file(os.path.join(neg_path, f), '0')
 
-    if MUL_AFTER_WINS: 
-        dp.T.to_csv('data/features_win_size_1_({})1.csv'.format(NUM_AFTER_WINS), header=True)
+    if MUL_AFTER_WINS:
+        dp.T.to_csv('data/features_win_size_{}_({}){}.csv'.format(WIN_SIZE_BEFORE, NUM_AFTER_WINS, WIN_SIZE_AFTER), header=True)
     else:
-        dp.T.to_csv('data/features_win_size_1_2_new_pos.csv'.format(WIN_SIZE_AFTER), header=True)
+        print(dp.T.shape)
+        dp.T.to_csv('data/features_win_size_{}_{}_new_pos.csv'.format(WIN_SIZE_BEFORE, WIN_SIZE_AFTER), header=True)
         # instead of transpose, do this: http://stackoverflow.com/questions/10715965/add-one-row-in-a-pandas-dataframe
 
 
-# ? run featurizer per person 
+# ? run featurizer per person
 # argparse w/ arguments: input and output directories.
 
 
