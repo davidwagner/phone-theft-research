7aceebdbfe6e489bbbdbbee2ddfafbe3857228e0
wonpark <won.park19@gmail.com>
2017-12-08 15:39:10 -0800

    Edited to allow for training of different models

--- classifiers-scripts/backpack_classifier_nn.py
+++ classifiers-scripts/backpack_classifier_nn.py
@@ -1,6 +1,6 @@
 import keras
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPooling1D, GlobalAveragePooling1D
 
 from sklearn.model_selection import train_test_split
 
@@ -9,58 +9,64 @@ import csv
 import os
 import argparse
 
-POSITIVE_DATA = "Backpack_PostData"
-NEGATIVE_DATA = "Backpack_NegData"
+POSITIVE_DATA = "../../Backpack_PostData"
+NEGATIVE_DATA = "../../Backpack_NegData"
 
-VALIDATION_FOLDER = "validation_backpack"
+VALIDATION_FOLDER = "../../validation_backpack"
 
 FEATURES_FILE = "BackpackFeatures.npz"
 VALIDATION_FILE = "backpack_validation.npz"
 
 CLASSIFIER_NAME = "BackpackClassifierNN.h5"
 
-WINDOW_SIZE = 50
+WINDOW_SIZE = 100
+MAX_TRIM = 2500
 
 def create_training_data():
 
-	positive_features = []
-	negative_features = []
+	full_feature_vector = []
+	all_labels = []
 
 	for folder in [POSITIVE_DATA, NEGATIVE_DATA]:
 
-		full_feature_vector = []
-
 		for filename in os.listdir(folder):
-			print(filename)
+
 			if not filename.endswith(".csv"):
 				continue
 			with open(folder + "/" + filename, 'r') as datafile:
+				print(filename)
+
 				all_data = list(csv.reader(datafile))
 
+				if os.path.isfile(folder + "/" + filename[:-4] + "_trim.txt"):
+					fp = open(folder + "/" + filename[:-4] + "_trim.txt")
+					trim = int(fp.readline())
+
+					all_data = all_data[:trim]
 			
-			
-			for i in range(len(all_data) - WINDOW_SIZE + 1):
+			for i in range(min(MAX_TRIM, len(all_data)) - WINDOW_SIZE + 1):
+
 				feature_vector = []
 				for j in range(WINDOW_SIZE):
 					if len(all_data[i+j]) < 4:
 						break
-					feature_vector.append(float(all_data[i + j][1]))
-					feature_vector.append(float(all_data[i + j][2]))
-					feature_vector.append(float(all_data[i + j][3]))
 
-				if len(feature_vector) == WINDOW_SIZE * 3:
-					full_feature_vector.append(feature_vector)
+					depth_vector = []
+					depth_vector.append(float(all_data[i + j][1]))
+					depth_vector.append(float(all_data[i + j][2]))
+					depth_vector.append(float(all_data[i + j][3]))
 
-		if folder == POSITIVE_DATA:
-			positive_features = full_feature_vector
-		else:
-			negative_features = full_feature_vector
+					feature_vector.append(depth_vector)
 
+				full_feature_vector.append(feature_vector)
 
-
+				if folder == POSITIVE_DATA:
+					all_labels.append(1)
+				else:
+					all_labels.append(0)
 	#Save to .npz
-	# vals_to_save = {temp_features[0]:"positive", temp_features[1]:"negative"}
-	np.savez(FEATURES_FILE, positive_features = positive_features, negative_features = negative_features)
+	print(np.shape(full_feature_vector))
+	np.savez(FEATURES_FILE, full_feature_vector = full_feature_vector, all_labels = all_labels)
 
 def create_validation_data(isPositive=True):
 	data = []
@@ -101,48 +107,56 @@ def create_validation_data(isPositive=True):
 def train_model(args):
 
 	#Read from .npz
-	feature_vectors = np.load(FEATURES_FILE)
+	data = np.load(FEATURES_FILE)
 
-	positive_features = feature_vectors['positive_features']
-	negative_features = feature_vectors['negative_features']
+	features = data['full_feature_vector']
+	labels = data['all_labels']
 
-	full_data = np.concatenate((positive_features, negative_features))
+	#Shuffle_Data
+	# Generate the permutation index array.
+	permutation = np.random.permutation(features.shape[0])
+	# Shuffle the arrays by giving the permutation in the square brackets.
+	shuffled_features = features[permutation]
+	shuffled_labels = labels[permutation]
 
-	labels = [1] * len(positive_features)
-	labels.extend([0] * len(negative_features))
-
-	labels = np.reshape(labels, (len(labels),1))
-
-	#Split data randomly for validation/training
-	data_train, data_test, labels_train, labels_test = train_test_split(full_data, labels, \
-														test_size=0.20)
-
-	#Recurrent, 1D Convluitional 
+	# shuffled_features = np.reshape(shuffled_features, (len(shuffled_features),WINDOW_SIZE, 3))
 
 	repeat_scores = []
 
 	for _ in range(args.repeat):
 
 		# create model
-		model = Sequential()
-		model.add(Dense(12, input_dim=WINDOW_SIZE * 3, activation='relu'))
-		model.add(Dense(8, activation='relu'))
-		model.add(Dense(1, activation='sigmoid'))
+		if args.model_num == 1:
+			model = Sequential()
+			model.add(Dense(12, input_shape=(WINDOW_SIZE,3), activation='relu'))
+			model.add(Flatten())
+			model.add(Dense(8, activation='relu'))
+			model.add(Dense(1, activation='sigmoid'))
+		else:
+			model = Sequential()
+			model.add(Conv1D(64, 3, activation='relu', input_shape=(WINDOW_SIZE, 3)))
+			model.add(Conv1D(64, 3, activation='relu'))
+			model.add(MaxPooling1D(3))
+			model.add(Conv1D(128, 3, activation='relu'))
+			model.add(Conv1D(128, 3, activation='relu'))
+			model.add(GlobalAveragePooling1D())
+			model.add(Dropout(0.5))
+			model.add(Dense(1, activation='sigmoid'))
 
 		#compile
 		model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-		model.fit(data_train, labels_train, epochs=args.epochs, batch_size=10)
+		model.fit(shuffled_features, shuffled_labels, epochs=args.epochs, batch_size=10, validation_split = 0.2)
 		#Save model
 
 
-		#Evaluate model
-		scores = model.evaluate(data_test, labels_test)
-		print(scores)
-		repeat_scores.append(scores[1])
-		print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+	# 	#Evaluate model
+	# 	scores = model.evaluate(data_test, labels_test)
+	# 	print(scores)
+	# 	repeat_scores.append(scores[1])
+	# 	print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-	print(np.mean(repeat_scores), np.std(repeat_scores))
+	# print(np.mean(repeat_scores), np.std(repeat_scores))
 	model.save(CLASSIFIER_NAME)
 
 def evaluate_classifier():
@@ -177,7 +191,7 @@ def parse_args():
 
   parser.add_argument(
 	'-repeat', metavar='repeat', dest='repeat', type=int,
-	default=3,  
+	default=1,  
 	help="Number of repeats.",
   )
   parser.add_argument(
@@ -186,8 +200,8 @@ def parse_args():
   )
 
   parser.add_argument(
-	'-data', metavar='data', dest='data', type=str,
-
+	'-model_num', metavar='model_num', dest='model_num', type=int,
+	required=True
   )
 
   args = parser.parse_args()
